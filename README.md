3c6f1fe04701e820472705d1edc511ea133abbb4
Alexey Goloburdin <terrarium-a@yandex.ru>
2022-11-16 17:27:45 +0300

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# @donate Telegram bot addition
+
+You can use [@donate](https://t.me/donate) Telegram bot to create paid Telegram channels available by subscription. And [@donate](https://t.me/donate) will manage the accounts in that channel, but not in linked with channel chat. This small Python script can ban users in chat, who are not in channel now.
+
+To install create new virtual environment and install requirements ([httpx](https://www.python-httpx.org/) and [telethon](https://docs.telethon.dev/en/stable/)).
+
+```bash
+python3 -m venv env
+source ./env/bin/activate
+pip install -r requirements.txt
+```
+
+Than create telegram bot with [@BotFather](https://t.me/BotFather). [Documentation](https://core.telegram.org/bots/tutorial) from Telegram. Remember Bot token.
+
+Then [create Telegram app](https://my.telegram.org/), «API development tools» section. Remember `api_id` and `api_hash`.
+
+Then find out your channel id, remember it. You can see it in URL on Telegram web version, for example, our channel has url `https://web.telegram.org/z/#-1251775798`, so `-1251775798` is our id, but we need add `100` to the start, so we have id: `-1001251775798`.
+
+Add bot as admin to your channel and his linked chat.
+
+Run script with activated virtual environment:
+
+```bash
+source ./env/bin/activate
+
+TG_API_ID=... \
+    TG_API_HASH=... \
+    TG_BOT_TOKEN=... \
+    TG_CHANNEL_ID=... \
+    python fix-donate-tg-chat.py
+```
+
+`TG_API_ID` and `TG_API_HASH` — from [my.telegram.org](https://my.telegram.org). `TG_BOT_TOKEN` from [@BotFather](https://t.me/BotFather). `TG_CHANNEL_ID` is your channel id, `-1001251775798` for our Telegram channel.
+
+You can add this script to cron scheduler for everyday run.
---
+++ fix-donate-tg-chat.py
@@ -0,0 +1,128 @@
+"""
+Removes from Telegram channel's chat users that in chat but not in channel.
+Can use with @donate subscription channels.
+"""
+import logging
+import os
+import time
+
+import httpx
+from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import User
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+try:
+    # api_id and api_hash from my.telegram.org, «API development tools» section
+    TG_API_ID = int(os.environ["TG_API_ID"])
+    TG_API_HASH = os.environ["TG_API_HASH"]
+    # bot_token from Telegram's @BotFather
+    TG_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
+    # Id of your telegram channel. You can see it in web.telegram.org/z/,
+    # add -100 to the string start.
+    # For example, id in URL is 123, so write -100123 here
+    TG_CHANNEL_ID = int(os.environ["TG_CHANNEL_ID"])
+    logger.debug("Env variables loaded")
+    logger.debug(f"Work with channel_id {TG_CHANNEL_ID}")
+except (KeyError, ValueError):
+    logger.critical(
+        "Please, set correct env variables: \n"
+        "  * TG_API_ID\n  * TG_API_HASH\n  * TG_BOT_TOKEN\n  * TG_CHANNEL_ID")
+    raise
+
+
+
+def get_tg_url(method: str, **params):
+    """Returns URL for Telegram Bot API method `method`
+    and optional key=value `params`"""
+    url = f"https://api.telegram.org/bot{TG_BOT_TOKEN}/{method}"
+    if params:
+        params = "&".join([f"{key}={value}" for key, value in params.items()])
+        url += "?" + params
+    return url
+
+
+def get_telegram_chat_id_by_channel_id(channel_id: int) -> int:
+    """Returns chat id for channel's linked chat"""
+    url = get_tg_url(method="getChat", chat_id=channel_id)
+    json_response  = httpx.get(url).json()
+    return json_response["result"]["linked_chat_id"]
+
+
+def is_user_in_channel(user_id: int, channel_id: int) -> bool:
+    """Returns True if user `user_id` in `channel_id` now"""
+    url = get_tg_url(method="getChatMember",
+            chat_id=channel_id, user_id=user_id)
+    json_response  = httpx.get(url).json()
+    try:
+        return json_response["result"]["status"] == "member"
+    except KeyError:
+        return False
+
+
+def get_all_chat_users(chat_id: int) -> list[User]:
+    """Returns list of all chat users by `chat_id`"""
+    logger.debug(f"Load users for chat {chat_id}")
+    client = TelegramClient('bot', TG_API_ID, TG_API_HASH)\
+            .start(bot_token=TG_BOT_TOKEN)
+    all_users = []
+    with client:
+        chat = client.get_entity(chat_id)
+        limit = 100
+        index = 0
+        while True:
+            users = client(GetParticipantsRequest(
+                chat, filter=chat,  # type: ignore
+                offset=index * limit, limit=limit, hash=0)
+            ).users  # type: ignore
+            if not users:
+                break
+            all_users.extend(users)
+            index += 1
+    return all_users
+
+
+def ban_user_from_chat(user_id: int, chat_id: int):
+    """Ban user `user_id` in chat `chat_id`"""
+    url = get_tg_url(method="banChatMember", user_id=user_id, chat_id=chat_id)
+    httpx.get(url).json()
+
+
+def unban_user_from_chat(user_id: int, chat_id: int):
+    """Unban user `user_id` in chat `chat_id`"""
+    url = get_tg_url(method="unbanChatMember", user_id=user_id, chat_id=chat_id)
+    httpx.get(url).json()
+
+
+def main():
+    TG_CHAT_ID = get_telegram_chat_id_by_channel_id(TG_CHANNEL_ID)
+    logger.debug(f"Linked chat id is {TG_CHAT_ID}")
+
+    users_in_chat = get_all_chat_users(TG_CHAT_ID)
+    users_in_chat_count = len(users_in_chat)
+    logger.debug(f"Users in chat: {users_in_chat_count}")
+
+    for index, user in enumerate(users_in_chat):
+        if user.bot: continue  # Skip bots
+        is_channel_member = is_user_in_channel(user.id, TG_CHANNEL_ID)
+        if not is_channel_member:
+            logger.info(
+                f"We need to ban+unban user #{user.id}, "
+                f"username {user.username}")
+            # ban_user_from_chat(user.id, TG_CHAT_ID)
+            # unban_user_from_chat(user.id, TG_CHAT_ID)
+        time.sleep(0.5)
+        if index % 10 == 0:
+            logger.debug(f"Processed {index} of {users_in_chat_count} users")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.exception("Uncaught exception")
